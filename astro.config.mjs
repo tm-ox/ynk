21bd4f36fa7202bdc714c331632c6de2c817fec8
tm <tm.oxenfree@gmail.com>
2024-04-04 11:03:02 +0700

    switch back from partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'
 
-import alpinejs from "@astrojs/alpinejs";
+// import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }),]
+  })]
 });
