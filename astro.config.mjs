c8c2761941995b215ccde941478f070947e837cb
tm <tm.oxenfree@gmail.com>
2024-04-04 10:22:43 +0700

    switch gtag to partytpwn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import partytown from '@astrojs/partytown'
 
-// import alpinejs from "@astrojs/alpinejs";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
